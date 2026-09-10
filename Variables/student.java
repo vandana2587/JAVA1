1bbdd3087b7620c2eb3586c67fb2c182d3e21564
Vandana Patel <vandanapatel@Vandanas-MacBook-Air.local>
2026-09-10 22:54:26 +0530

    feat: added student.java

---
+++ Variables/student.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class student {
+    public static void main(String[] args) {
+        String name="vandana";
+        int rollno=53;
+        String percentage="78%";
+        System.out.println(name);
+        System.out.println(rollno);
+        System.out.println(percentage);
+
+
+    }
+    
+}
