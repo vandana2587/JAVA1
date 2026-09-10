ab8ace488f71f8063c866d888cb4bda2de163895
Vandana Patel <vandanapatel@Vandanas-MacBook-Air.local>
2026-09-10 23:36:26 +0530

    feat: added student.java

---
+++ Variables/average.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class average {
+    public static void main(String[] args) {
+        int a=5;
+        int b=57;
+        int c=590;
+        int avg=(a+b+c)/3;
+        System.out.println(avg);
+    }
+    
+}
---
+++ Variables/conversion.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class conversion {
+    public static void main(String[] args) {
+        int a=10;
+        double b=5.6;
+        int sum=(int)(a+b);
+        System.out.println(sum);
+    }
+    
+}
