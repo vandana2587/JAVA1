703137679429e660e9fdab97024e5b32b84cd4ff
Vandana Patel <vandanapatel@Vandanas-MacBook-Air.local>
2026-09-10 23:01:01 +0530

    feat: added student.java

--- Variables/temperature.java
+++ Variables/temperature.java
@@ -1,5 +1,10 @@
 package Variables;
 
 public class temperature {
+    public static void main(String[] args) {
+        int c=56;
+        int fahreniet=(c*9/5)+32;
+        System.out.println(fahreniet);
+    }
     
 }
