3ca197cff7efc4d14271a6da73387ea8c79645fb
Vandana Patel <vandanapatel@Vandanas-MacBook-Air.local>
2026-09-10 23:29:52 +0530

    feat: added student.java

---
+++ Variables/intrest.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class intrest {
+    public static void main(String[] args) {
+        int p=56;
+        int r=23;
+        int t=34;
+        int SI=(p*r*t/100);
+        System.out.println(SI);
+    }
+}
---
+++ Variables/swapping.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class swapping {
+    public static void main(String[] args) {
+        int a=6;
+        int b=2;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
