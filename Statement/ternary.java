202f5e7a255b3e43334a4be035e024dfc9d3eff9
Vandana Patel <vandanapatel@Vandanas-MacBook-Air.local>
2026-09-12 13:45:13 +0530

    feat: added student.java

---
+++ Statement/ternary.java
@@ -0,0 +1,11 @@
+package Statement;
+
+public class ternary {
+    public static void main(String[] args) {
+        int marks= 20;
+        String result = (marks >= 18) ? "pass" : "fail";
+        System.out.println(result);
+    }
+
+    
+}
